90d9a5cc80c13f3bbcc2a6f5e0422d9180225a66
Amash Chaudhry <thug.amash@gmail.com>
2024-08-19 16:57:20 +0500

    Second Commit

---
+++ lib/src/features/authentications/screens/splash_screen/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:authentications/src/constants/colors.dart';
+import 'package:authentications/src/features/authentications/screens/login/login_screen.dart';
+import 'package:authentications/src/features/core/screens/home/home_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void checkAuthentication() {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      if (user.emailVerified) {
+        Get.offAll(() => const HomeScreen());
+      } else {
+        Get.offAll(() => const HomeScreen());
+      }
+    } else {
+      Get.offAll(() => const LoginScreen());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500), () {
+      checkAuthentication();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: pageBackgroundColor,
+    );
+  }
+}
